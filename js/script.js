eda6fc0e71a2009017bfeaf1abb182c960d2f43a
Nataliia Radina <shyianovska@gmail.com>
2016-01-12 11:18:22 +0100

    map improve

--- js/script.js
+++ js/script.js
@@ -8,26 +8,26 @@ function showMenu() {
 	document.getElementById("mobile-menu").style.marginTop = "0";
 	document.getElementById("menu-button").style.marginTop = "500px";
 	document.getElementById("logo-green").style.display = "block";
-}	
+}
+	
 function hideMenu() {
 	document.getElementById("mobile-menu").style.marginTop = "-510px";
 	document.getElementById("menu-button").style.marginTop = "0";	
 	document.getElementById("logo-green").style.display = "none";
 }
 
-function showSpeakers() {
+function showSpeakers(e) {
+	e.preventDefault();
 	var speakers = document.getElementsByClassName("hidden-speaker");
 	if (document.getElementById("btn-bold").innerHTML == "All") {
 		document.getElementById("btn-bold").innerHTML = "Hide";
 		for  (i = 0; i < speakers.length; i++) {
 			speakers[i].style.display = "inline-block";
-		}
-		
+		}	
 	} else {
 		for  (i = 0; i < speakers.length; i++) {
 			speakers[i].style.display = "none";
 		}
 		document.getElementById("btn-bold").innerHTML = "All"
 	}
-
 }
