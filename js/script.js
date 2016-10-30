3d94b361786f0f954aab2c0c055111e2033cb7bd
Nataliia Radina <shyianovska@gmail.com>
2016-10-30 22:49:12 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 window.addEventListener('load', function() {
 	document.getElementById("menu-button").addEventListener("click", showMenu);
 	document.getElementById("exit").addEventListener("click", hideMenu);
-	document.getElementById("speakers-button").addEventListener("click", showSpeakers);
 	$('.single-item').slick({
 		infinite: true,
 		speed: 300,
 		slidesToShow: 1,
 		autoplay: true
 	});
+	$('.organizer-block.random').shuffle();
 });
 
-function showMenu() {	
+function showMenu() {
 	document.getElementById("mobile-menu").style.marginTop = "0";
-	document.getElementById("menu-button").style.marginTop = "500px";
+	document.getElementById("menu-button").style.marginTop = "700px";
 	document.getElementById("logo-green").style.display = "block";
 }
-	
+
 function hideMenu() {
-	document.getElementById("mobile-menu").style.marginTop = "-510px";
-	document.getElementById("menu-button").style.marginTop = "0";	
+	document.getElementById("mobile-menu").style.marginTop = "-710px";
+	document.getElementById("menu-button").style.marginTop = "0";
 	document.getElementById("logo-green").style.display = "none";
 }
-
 
 function showSpeakers(e) {
 	e.preventDefault();
@@ -30,7 +29,7 @@ function showSpeakers(e) {
 		document.getElementById("btn-bold").innerHTML = "Hide";
 		for  (i = 0; i < speakers.length; i++) {
 			speakers[i].style.display = "inline-block";
-		}	
+		}
 	} else {
 		for  (i = 0; i < speakers.length; i++) {
 			speakers[i].style.display = "none";
@@ -39,3 +38,21 @@ function showSpeakers(e) {
 	}
 }
 
+(function($){
+	$.fn.shuffle = function() {
+		var allElems = this.get(),
+			getRandom = function(max) {
+				return Math.floor(Math.random() * max);
+			},
+			shuffled = $.map(allElems, function(){
+				var random = getRandom(allElems.length),
+					randEl = $(allElems[random]).clone(true)[0];
+				allElems.splice(random, 1);
+				return randEl;
+			});
+		this.each(function(i){
+			$(this).replaceWith($(shuffled[i]));
+		});
+		return $(shuffled);
+	};
+})(jQuery);
